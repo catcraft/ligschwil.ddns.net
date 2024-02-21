47b1dd456e11981a3c7f8fc4ffd382b839d69f87
catcraft <72657021+catcraft@users.noreply.github.com>
2024-02-21 22:18:25 +0100

    Changed back to port 80

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ cron.schedule('0 5 * * *', () => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
